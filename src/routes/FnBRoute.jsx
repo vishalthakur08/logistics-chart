e96368c560c36d6d12383f32d8fb882fbaa38457
Vishal Thakur <thakurvishal031@gmail.com>
2025-09-25 12:57:08 +0400

    chore: temporarily remove FnBRoute to fix build

--- src/routes/FnBRoute.jsx
+++
@@ -1,6 +0,0 @@
-// src/routes/FnBRoute.jsx
-import React from "react";
-import FnbRoasChart from "../charts/FnbROASChart";
-export default function FnBRoute() {
-  return <FnbRoasChart width={280} height={228} />;
-}
